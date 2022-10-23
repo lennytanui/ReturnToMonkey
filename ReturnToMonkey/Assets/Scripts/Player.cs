c904cf417599d65c984b19efd9e84a083af2eca7
lennytanui <lennytanui@outlook.com>
2022-10-22 23:49:50 -0500

    started work on ladders

--- ReturnToMonkey/Assets/Scripts/Player.cs
+++ ReturnToMonkey/Assets/Scripts/Player.cs
@@ -7,15 +7,37 @@ public class Player : MonoBehaviour
     public Animator animator;
     private CharacterController2D characterController2D;
     public float speed = 0;
+    private bool isClimbing = false;
     // Start is called before the first frame update
     void Start()
     {
         characterController2D = this.GetComponent<CharacterController2D>();
     }
+    
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("ladder")){
+            Debug.Log("Colliding with ladder");
+            isClimbing = true;
+        }
+    }
 
+    void OnTriggerExit2D(Collider2D collision){
+        if(collision.CompareTag("ladder")){
+            Debug.Log("Player Exited the Ladder");
+            isClimbing = false;
+        }
+    }
+    void LadderMovement(){
+        
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        if(isClimbing){
+            // working on climbing
+        }
+
         if(Input.GetKey(KeyCode.D)){
             characterController2D.Move(speed * Time.deltaTime, false, false);
         }
