b7b79ee65ba3dc06b5614e71cff169e3d7af82b2
AndersonLoan <97067748+AndersonLoan@users.noreply.github.com>
2022-10-26 15:54:34 -0500

    more animation stuff

---
+++ ReturnToMonkey/Assets/Scripts/Interactable.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    static public bool InRange;
+    static public int form;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        form = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(InRange)
+        {
+            if(Input.GetKey(KeyCode.M))
+            {
+                form += 1;
+                if(form == 2)
+                {
+                    form = 0;
+                }
+            }
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"));
+        InRange = true;
+        Debug.Log("Player is in range");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"));
+        InRange = false;
+        Debug.Log("Player is not in range");
+    }
+}
--- ReturnToMonkey/Assets/Scripts/Player.cs
+++ ReturnToMonkey/Assets/Scripts/Player.cs
@@ -16,16 +16,41 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        animator.SetFloat("Speed", 0);
         if(Input.GetKey(KeyCode.D)){
             characterController2D.Move(speed * Time.deltaTime, false, false);
+            animator.SetFloat("Speed",1);
         }
 
         if(Input.GetKey(KeyCode.A)){
             characterController2D.Move(-1 * speed * Time.deltaTime, false, false);
+            animator.SetFloat("Speed",1);
         }
 
         if(Input.GetKey(KeyCode.Space)){
             characterController2D.Move(0, false, true);
+            animator.SetBool("IsJumping", true);
         }
+        if(Input.GetKey(KeyCode.LeftShift)){
+            characterController2D.Move(0, true, false);
+            animator.SetBool("IsCrouching", true);
+        }
+        if(Interactable.form == 1)
+        {
+            animator.SetBool("isFrog", true);
+            
+        }
+        if(Interactable.form == 0)
+        {
+            animator.SetBool("isFrog", false);
+        }
+    }
+    public void OnLandEvent()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+    public void OnCrouchEvent()
+    {
+        animator.SetBool("IsCrouching", false);
     }
 }
