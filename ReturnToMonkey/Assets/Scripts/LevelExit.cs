48d8c9e0c8716fcb4a9e4cb5b8e93ae5d55428ae
lukeGallucci <lukegallucci@tamu.edu>
2022-11-07 23:19:36 -0600

    Luke/ExitUpdate
    Updated the function and use of the exit, to have the screen fade to black

--- ReturnToMonkey/Assets/Scripts/LevelExit.cs
+++ ReturnToMonkey/Assets/Scripts/LevelExit.cs
@@ -7,9 +7,30 @@ using UnityEngine.SceneManagement;
 public class LevelExit : MonoBehaviour
 {
     public float levelLoadDelay = 1f;
+    public SpriteRenderer BlackBoxSprite;
+    public float fadeSpeed = 1;
+    public Player player;
+    public Camera camera;
+
+    private Color changeFade;
+    private bool canFade;
+
+    private void Start()
+    {
+        canFade = false;
+        changeFade = new Color(0f, 0f, 0f, fadeSpeed);
+    }
+    private void Update()
+    {
+        if (canFade)
+        {
+            FadeToBlack();
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         if (other.tag =="Player")
         {
             StartCoroutine(LoadNextLevel());
@@ -18,6 +39,13 @@ public class LevelExit : MonoBehaviour
 
     IEnumerator LoadNextLevel()
     {
+        canFade = true;
+        //canMove = false
+        //if (canMove) {
+        //NORMAL MOVEMENT
+        //else {
+        //horizontalMove = runSpeed;
+        //}
         yield return new WaitForSecondsRealtime(levelLoadDelay);
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
@@ -29,4 +57,15 @@ public class LevelExit : MonoBehaviour
         SceneManager.LoadScene(nextSceneIndex);
     }
 
+    
+
+    private void FadeToBlack()
+    {
+        BlackBoxSprite.color += changeFade * Time.deltaTime;
+    }
+
+    private void StopCamera()
+    {
+        BlackBoxSprite.color += changeFade * Time.deltaTime;
+    }
 }
