3b186c7486948e3ef0872a6e1196add234cd6186
lennytanui <lennytanui@outlook.com>
2022-11-24 21:02:31 -0600

    Revert "Merge pull request #26 from lennytanui/Luke/fade_update"
    This reverts commit 5129d8dc65af3b0edfbfadbce0f8a93f90a194e0, reversing
    changes made to c89beaac05de3357f19f292118b48a7b149b2cc4.

--- ReturnToMonkey/Assets/Scripts/LevelExit.cs
+++ ReturnToMonkey/Assets/Scripts/LevelExit.cs
@@ -7,14 +7,31 @@ using UnityEngine.SceneManagement;
 public class LevelExit : MonoBehaviour
 {
     public float levelLoadDelay = 1f;
-    public Fade fade;
+    public SpriteRenderer BlackBoxSprite;
+    public float fadeSpeed = 1;
     public Player player;
     public CinemaMachineSwitcher cameraScript;
 
+    private Color changeFade;
+    private bool canFade;
+
+    private void Start()
+    {
+        canFade = false;
+        changeFade = new Color(0f, 0f, 0f, fadeSpeed);
+    }
+    private void Update()
+    {
+        if (canFade)
+        {
+            FadeToBlack();
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-
-        if (other.tag == "Player")
+        
+        if (other.tag =="Player")
         {
             StartCoroutine(LoadNextLevel());
         }
@@ -23,8 +40,13 @@ public class LevelExit : MonoBehaviour
     IEnumerator LoadNextLevel()
     {
         cameraScript.SwitchToExitState();
-        fade.FadeOut();
+        canFade = true;
         player.moveRight = true;
+        //if (canMove) {
+        //NORMAL MOVEMENT
+        //else {
+        //horizontalMove = runSpeed;
+        //}
         yield return new WaitForSecondsRealtime(levelLoadDelay);
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         int nextSceneIndex = currentSceneIndex + 1;
@@ -36,4 +58,10 @@ public class LevelExit : MonoBehaviour
         SceneManager.LoadScene(nextSceneIndex);
     }
 
+    
+
+    private void FadeToBlack()
+    {
+        BlackBoxSprite.color += changeFade * Time.deltaTime;
+    }
 }
